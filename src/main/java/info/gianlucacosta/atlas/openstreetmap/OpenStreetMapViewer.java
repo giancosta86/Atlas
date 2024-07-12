ff01df14fee074d59c949323bf59b75e7408e530
Gianluca Costa <gianluca@gianlucacosta.info>
2024-07-12 15:25:41 +0200

    Update map mechanism

--- src/main/java/info/gianlucacosta/atlas/openstreetmap/OpenStreetMapViewer.java
+++ src/main/java/info/gianlucacosta/atlas/openstreetmap/OpenStreetMapViewer.java
@@ -3,6 +3,9 @@ package info.gianlucacosta.atlas.openstreetmap;
 import info.gianlucacosta.atlas.openstreetmap.tiles.OpenStreetMapTileFactory;
 import info.gianlucacosta.zephyros.swing.components.InteractiveHtmlPane;
 import org.jxmapviewer.JXMapViewer;
+import org.jxmapviewer.OSMTileFactoryInfo;
+import org.jxmapviewer.viewer.DefaultTileFactory;
+import org.jxmapviewer.viewer.TileFactoryInfo;
 
 import java.awt.Graphics;
 
@@ -19,29 +22,24 @@ import java.awt.Graphics;
 public class OpenStreetMapViewer extends JXMapViewer {
     private final InteractiveHtmlPane attributionPane;
 
-    public OpenStreetMapViewer(OpenStreetMapTileFactory tileFactory) {
+    public OpenStreetMapViewer() {
+        TileFactoryInfo info = new OSMTileFactoryInfo();
+        DefaultTileFactory tileFactory = new DefaultTileFactory(info);
+
         setTileFactory(
-                tileFactory
-        );
+                tileFactory);
 
-        String attribution =
-                tileFactory
-                        .getOpenStreetMapTileFactoryInfo()
-                        .getAttribution();
+        String attribution = "";
 
-        attributionPane =
-                new InteractiveHtmlPane(
-                        attribution
-                );
+        attributionPane = new InteractiveHtmlPane(
+                attribution);
 
         add(attributionPane);
     }
 
-
     public OpenStreetMapTileFactory getOpenStreetMapTileFactory() {
         return (OpenStreetMapTileFactory) getTileFactory();
     }
-
 
     /**
      * @return The zoom level according to OpenStreetMap's convention
@@ -50,19 +48,16 @@ public class OpenStreetMapViewer extends JXMapViewer {
         return getTileFactory().getInfo().getTotalMapZoom() - getZoom();
     }
 
-
     /**
      * Sets the zoom level according to OpenStreetMap's convention
      *
      * @param openStreetMapZoom Zoom level. 0 = world-level zoom
      */
     public void setOpenStreetMapZoom(int openStreetMapZoom) {
-        int internalZoom =
-                getTileFactory().getInfo().getTotalMapZoom() - openStreetMapZoom;
+        int internalZoom = getTileFactory().getInfo().getTotalMapZoom() - openStreetMapZoom;
 
         setZoom(internalZoom);
     }
-
 
     @Override
     protected void paintComponent(Graphics g) {
@@ -70,7 +65,6 @@ public class OpenStreetMapViewer extends JXMapViewer {
 
         attributionPane.setLocation(
                 getWidth() - attributionPane.getWidth(),
-                getHeight() - attributionPane.getHeight()
-        );
+                getHeight() - attributionPane.getHeight());
     }
 }
